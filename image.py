45d27f40d942dcd3aed333c177a91ba9122b4e42
devalresourse <adeola.olanrewaju@gmail.com>
2018-11-19 22:32:12 -0500

    Add files via upload
    conditional statement

---
+++ image.py
@@ -0,0 +1,36 @@
+x = 120
+if x < 50:
+     print('x is small')
+else:
+     print('x is large')
+     
+'# log this'
+
+name = 'Adeola'
+salary = 2300
+days = 25
+if name == 'John':
+    print('Hello John')
+elif name == 'Lanre':
+    print('Hello Lanre')
+elif name == 'Adeola':
+    print('Welcome Adeola')
+else:
+    print('Contact Admin')
+"""
+name = {
+    'John': 'Hello John',
+    'Lanre': 'Hello Lanre',
+    'Adeola': 'Welcome Adeola'
+    }
+print(name.get('Hello Adeola'))
+"""
+
+def __init__(self, name, salary):
+      self.name = name
+      self.salary = salary
+      Employee.empCount += 1
+
+print(name, salary)
+print(salary / days)
+    
