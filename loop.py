8bad49f245d28f13c8d54a05144bf53fd61e2d65
devalresourse <adeola.olanrewaju@gmail.com>
2018-11-20 21:34:02 -0500

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,18 @@
+#print eath item in list
+
+pan = ["The Walking Dead", "Entourage", "The Sopranos", "The Vampire Diaries"]
+for pan in pan:
+    print(pan)
+
+#range
+
+for num in range(25,50):
+    print(num)
+
+pan = ["The Walking Dead", "Entourage", "The Sopranos", "The Vampire Diaries"]
+for index, show in enumerate(pan):
+    print(index)
+    print(pan)
+
+
+
